87f663af6869b95019c3ac3f6796856135f36426
Yan P Aung <yan.pye.aung@gmail.com>
2021-11-10 20:04:12 -0800

    Use @Body and @Param decorators

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,12 +8,12 @@ export class MessagesController {
   }
 
   @Post()
-  createMessage() {
-
+  createMessage(@Body() body: any) {
+    console.log(body);
   }
 
   @Get('/:id')
-  getMessage() {
-
+  getMessage(@Param('id') id: string) {
+    console.log(id);
   }
 }
